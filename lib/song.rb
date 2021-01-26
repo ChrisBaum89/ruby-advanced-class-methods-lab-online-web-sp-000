b2f1abbeb59cff004ca2210d2ba2f2dffb9a0567
Chris Baum <christopher.baum@valpo.edu>
2021-01-26 02:30:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.find{|x| x.name == name }
+    @@all.find{|x| x.name == name }  #searches all instances of the class to see if a name property matches the name
   end
 
   def self.find_or_create_by_name(name)
